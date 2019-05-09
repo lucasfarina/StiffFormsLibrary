64b64f02d0a8ea3cc1bb9fc1f0609225c392c457
Lucas Farina <lucas@garage227studios.com>
2019-05-09 18:19:35 -0300

    Initial Commit

---
+++ StiffFormsLibrary/HideableTabControl.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StiffFormsLibrary
+{
+    public partial class HideableTabControl : TabControl
+    {
+        public HideableTabControl()
+        {
+            InitializeComponent();
+            HiddenPages = new List<TabPage>();
+            lastIndex = new List<int>();
+        }
+
+        public List<TabPage> HiddenPages;
+        public List<int> lastIndex;
+
+        public void ShowPage(TabPage tabPage)
+        {
+            if (HiddenPages.Contains(tabPage))
+            {
+                int i = HiddenPages.FindIndex(x => x == tabPage);
+                TabPages.Insert(i, tabPage);
+                HiddenPages.RemoveAt(lastIndex[i]);
+                lastIndex.RemoveAt(i);
+            }
+        }
+
+        public void HidePage(TabPage tabPage)
+        {
+            if(TabPages.Contains(tabPage)){
+                int v = TabPages.IndexOf(tabPage);
+                HiddenPages.Add(tabPage);
+                lastIndex.Add(v);
+                TabPages.RemoveAt(v);
+            }
+        }
+    }
+}
---
+++ StiffFormsLibrary/PlaceHolderTextbox.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StiffFormsLibrary
+{
+    public partial class PlaceHolderTextbox : TextBox
+    {
+        public PlaceHolderTextbox()
+        {
+            InitializeComponent();
+            this.GotFocus += PlaceHolderTextbox_GotFocus;
+            this.LostFocus += PlaceHolderTextbox_LostFocus;
+        }
+
+        private string placeHolder;
+        private bool isPlaceHolding;
+        public string TextPlaceHolder
+        {
+            get { return placeHolder; }
+            set
+            {
+                placeHolder = value;
+                if (!String.IsNullOrEmpty(TextPlaceHolder))
+                {
+                    AddPlaceHolder();
+                }
+                else
+                {
+                    RemovePlaceHolder();
+                }
+            }
+        }
+
+        void AddPlaceHolder()
+        {
+            this.Text = TextPlaceHolder;
+            this.ForeColor = SystemColors.GrayText;
+            isPlaceHolding = true;
+        }
+
+        void RemovePlaceHolder()
+        {
+            this.Text = "";
+            this.ForeColor = SystemColors.WindowText;
+            isPlaceHolding = false;
+        }
+
+
+        private void PlaceHolderTextbox_GotFocus(object sender, EventArgs e)
+        {
+            if(isPlaceHolding)
+            {
+                RemovePlaceHolder();
+            }
+        }
+
+        private void PlaceHolderTextbox_LostFocus(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(this.Text) && !isPlaceHolding)
+            {
+                AddPlaceHolder();
+            }
+        }
+
+        
+    }
+}
